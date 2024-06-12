e01591f1683dc79d8a6e44237d820500e861116e
Antonio12152 <01romanovskiy01@gmail.com>
2024-06-13 01:25:55 +0200

    tags connected to backend

--- Master_Gaming_frontend/src/pages/Tag.jsx
+++ Master_Gaming_frontend/src/pages/Tag.jsx
@@ -1,11 +1,19 @@
-import gameposts from '../database/gameposts.json'
+//import gameposts from '../database/gameposts.json'
 import { useParams } from 'react-router-dom';
 import GamePosts from './GamePosts';
+import { useEffect, useState } from 'react';
 
 const Tag = () => {
-    const posts = gameposts.slice().reverse()
+    //const posts = gameposts.slice().reverse()
+    const [posts, setPosts] = useState([]);
     let { tag } = useParams();
-    
+    useEffect(() => {
+            // posts = gameposts.slice().reverse()
+            fetch('http://localhost:5000/gameposts')
+                .then(response => response.json())
+                .then(data => setPosts(data.slice().reverse()))
+                .catch(error => console.error('Error fetching data:', error));
+    }, []);
     const postsTag = posts.reduce((arr, post) => {
         if (post.tags && post.tags.includes(tag)) {
             arr.push(post)
--- Master_Gaming_frontend/src/pages/Tags.jsx
+++ Master_Gaming_frontend/src/pages/Tags.jsx
@@ -1,19 +1,27 @@
+import { useEffect, useState } from 'react';
 import TagsList from '../components/TagsList';
-import gameposts from '../database/gameposts.json'
+//import gameposts from '../database/gameposts.json'
 
 const Tags = () => {
-    const posts = gameposts
-    const tags = posts.reduce((arr, post) => {
-        if (post.tags && Array.isArray(post.tags)) {
-            post.tags.forEach(tag => {
-                if (!arr.includes(tag)) {
-                    arr.push(tag);
-                }
-            });
-        }
-        return arr;
+    // const posts = gameposts
+    // const tags = posts.reduce((arr, post) => {
+    //     if (post.tags && Array.isArray(post.tags)) {
+    //         post.tags.forEach(tag => {
+    //             if (!arr.includes(tag)) {
+    //                 arr.push(tag);
+    //             }
+    //         });
+    //     }
+    //     return arr;
+    // }, []);
+    const [tags, setTags] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:5000/tags')
+            .then(response => response.json())
+            .then(data => setTags(data))
+            .catch(error => console.error('Error fetching data:', error));
     }, []);
-
 
     return (
         <TagsList tags={tags.sort()} />
