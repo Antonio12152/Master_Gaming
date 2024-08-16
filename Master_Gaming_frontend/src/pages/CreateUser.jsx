c005e8964beb41ee9c14669bf80e2549d40e30b5
Antonio12152 <01romanovskiy01@gmail.com>
2024-08-16 13:00:43 +0200

    Create User preset

--- Master_Gaming_frontend/src/pages/CreateUser.jsx
+++ Master_Gaming_frontend/src/pages/CreateUser.jsx
@@ -41,15 +41,15 @@ const CreateUser = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label>Username:</label>
-                    <input type="text" value={name} onChange={(e) => setTitle(e.target.value)} required />
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
                 </div>
                 <div>
                     <label>Email:</label>
-                    <input type="text" value={email} onChange={(e) => setTitle(e.target.value)} required />
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 </div>
                 <div>
-                    <label>Email:</label>
-                    <input type="text" value={password} onChange={(e) => setTitle(e.target.value)} required />
+                    <label>Password:</label>
+                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} required />
                 </div>
                 <div>
                     <label>Image URL:</label>
@@ -57,7 +57,7 @@ const CreateUser = () => {
                 </div>
                 <div>
                     <label>About:</label>
-                    <textarea value={about} onChange={(e) => setText(e.target.value)} />
+                    <textarea value={about} onChange={(e) => setAbout(e.target.value)} />
                 </div>
                 
                 {error && <p style={{ color: 'red' }}>{error}</p>}
