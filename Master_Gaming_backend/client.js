4b1b057f27389fbfa71d4b6a42fa224d7f7b30ea
Antonio12152 <01romanovskiy01@gmail.com>
2024-09-03 01:08:00 +0200

    vercel + aiven test

--- Master_Gaming_backend/client.js
+++ Master_Gaming_backend/client.js
@@ -4,32 +4,19 @@ const { Pool } = require("pg");
 
 const connectionString = process.env.POOL
 
-const client = new Pool({
-    connectionString
-});
-// aiven
-// const client = new Pool({
-//     connectionString: process.env.connectionString,
-//     ssl: {
-//         ca: process.env.CA,
-//         rejectUnauthorized: true 
-//     },
-// });
+const config_aiven = {
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: `${process.env.CA}`,
+    },
+};
 
-// old aiven
-// const config_aiven = {
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     host: process.env.HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB,
-//     ssl: {
-//         rejectUnauthorized: true,
-//         ca: `${process.env.CA}`,
-//     },
-// };
-
-// const client = new pg.Client(config_aiven);
+const client = new Pool(config_aiven);
 
 async function connectClient() {
     try {
