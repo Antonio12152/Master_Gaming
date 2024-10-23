6dde349ba7795009365f2e95352cc58a2e1e76c8
Antonio12152 <01romanovskiy01@gmail.com>
2024-10-23 13:42:11 +0200

    small structure backend change

--- Master_Gaming_backend/index.js
+++ Master_Gaming_backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://master-gaming.netlify.app/'
+    'https://master-gaming.netlify.app'
 ];
 
 const corsOptions = {
